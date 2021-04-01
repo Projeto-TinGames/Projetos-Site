5c9db3a5244369b98ce7c746094d231b3ab598a9
RafaelPetr <69591645+RafaelPetr@users.noreply.github.com>
2021-04-01 10:19:06 -0300

    Reconfigurando mysql

--- server/mysqlconnection.js
+++ server/mysqlconnection.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
     "local":"localhost",
     "user": "root",
+    "password": "123456",
     "database": "abcash",
     multipleStatements: true
 })
