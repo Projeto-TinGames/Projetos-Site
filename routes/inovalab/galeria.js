d9ac6d1ebf820c561d74858c47302e827eb659f4
Eduardo Lemos Pedroso <42592412+Eduardo-LP@users.noreply.github.com>
2021-04-19 13:25:02 -0300

    adição das rotas

--- routes/inovalab/galeria.js
+++ routes/inovalab/galeria.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(process.cwd() + '/views/inovalab/Home.html');
+    res.sendFile(process.cwd() + '/views/inovalab/galeria.html');
  });
 
 module.exports = router;
