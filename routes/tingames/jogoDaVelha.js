26aae15e535c12cd6d3cd5cc539b5208a44337c5
Rafael Petry da Silva <petryrafael29@gmail.com>
2021-11-25 00:24:40 -0300

    Alterando caminho das pastas

--- routes/tingames/jogoDaVelha.js
+++ routes/tingames/jogoDaVelha.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(process.cwd() + '/views/tingames/JogoDaVelha.html');
+    res.sendFile(process.cwd() + '/views/tingames/JogoDaVelha/JogoDaVelha.html');
 });
 
 module.exports = router;
