10fafc844de3e87989a562be64586a2d54bf3b6a
Rafael Petry da Silva <69591645+RafaelPetr@users.noreply.github.com>
2022-07-05 18:55:36 -0300

    Delete routes directory

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-    res.sendFile(process.cwd() + "/views/home.html");
- });
-
-module.exports = router;
--- routes/robolab/patrocine.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-	res.sendFile(process.cwd() + "/views/RoboLab/patrocinadores.html");
-});
-
-module.exports = router;
