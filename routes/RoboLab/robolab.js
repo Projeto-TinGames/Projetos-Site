f7832cb0377c64c479a1230aaed17cd19020b89a
Rafael Petry da Silva <petryrafael29@gmail.com>
2023-01-17 08:38:59 -0300

    Adicionada nova página do RoboLab

--- routes/RoboLab/robolab.js
+++ routes/RoboLab/robolab.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', function(req, res){
     res.sendFile(process.cwd() + "/views/RoboLab/home.html");
 });
 
-router.get("/projetos", (req, res, next) => {
+router.get('/projetos', function(req, res){
     res.sendFile(process.cwd() + "/views/RoboLab/projetos.html");
+});
+
+router.get('/galeria', function(req, res){
+    res.sendFile(process.cwd() + "/views/RoboLab/galeria.html");
 });
 
 module.exports = router;
